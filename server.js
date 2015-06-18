7395f15e69fbf3c0447f86181205f0ebbfe521eb
John Wu <webmaster@leapoahead.com>
2015-06-19 00:00:32 +0800

    Initial Commit

---
+++ server.js
@@ -0,0 +1,25 @@
+'use strict';
+
+let app = require('koa')(),
+  router = require('koa-router')(),
+  json = require('koa-json')(),
+  thunkify = require('thunkify'),
+  pyshell = require('python-shell'),
+  run = thunkify(pyshell.run),
+  path = require('path');
+
+app.use(json);
+
+router.get('/', function *(next) {
+  let result = yield run('usonic.py', {
+    scriptPath: __dirname
+  });
+  this.body = { distance: result[0] };
+  yield next;
+});
+
+app.
+  use(router.routes()).
+  use(router.allowedMethods());
+app.listen(3001);
+console.log('[Distance] Server now running at port 3001');
